a95e893433d51c7c094aab6dc62d06cfd6958088
Justin Roberts <justin.roberts@careerbuilder.com>
2014-11-26 10:48:06 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,4 @@ description       "SCM checkout, bundle install, and restart Sinatra web applica
 depends           "deploy"
 recipe            "sinatra::configure", "one time, makes all the deploy folders"
 recipe            "sinatra::deploy", "uses scm to update the codebase, bundle installs, and restarts the server"
+version           "0.1"
